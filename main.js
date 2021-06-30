574c3905774e17f10aaf49b878e8d925e3f985b3
alejcarr <85958375+alejcarr@users.noreply.github.com>
2021-06-30 19:41:06 -0400

    "Created  sentence segments for the msg in arrays"

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+// Create random message segments [at least three data points]
+var horoscopeSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces', ];
+var actionVerb = ['will be', 'will never be', 'wished they were', 'would hate to be', 'are', 'were', 'wanted to be', 'never wanted to be'];
+var lastSegement = ['a lawyer', 'a signer', 'a software engineer', 'a doctor', 'a superhero', 'a firefighter', 'an influencer', 'a plumber'];
+
+console.log(horoscopeSigns.length);
+console.log(actionVerb.length);
+console.log(lastSegement.length);
