5e758e5bab285d52e724ae7b097f6d096c9debc6
alejcarr <85958375+alejcarr@users.noreply.github.com>
2021-06-30 20:03:30 -0400

    Created sentence obj factory fn to organize arrs

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 // Create random message segments [at least three data points]
 var horoscopeSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces', ];
-var actionVerb = ['will be', 'will never be', 'wished they were', 'would hate to be', 'are', 'were', 'wanted to be', 'never wanted to be'];
-var lastSegement = ['a lawyer', 'a signer', 'a software engineer', 'a doctor', 'a superhero', 'a firefighter', 'an influencer', 'a plumber'];
+var msgVerb = ['will be', 'will never be', 'wished they were', 'would hate to be', 'are', 'were', 'wanted to be', 'never wanted to be'];
+var jobTitle = ['a lawyer', 'a signer', 'a software engineer', 'a doctor', 'a superhero', 'a firefighter', 'an influencer', 'a plumber'];
 
-console.log(horoscopeSigns.length);
-console.log(actionVerb.length);
-console.log(lastSegement.length);
+// Factory function that generates a sentence object based on three main portions of a sentence
+const sentenceFactory = (senSubject, senVerb, senObject) => ({
+    // assigns properties to arrays
+    _senSubject: senSubject,
+    _senVerb: senVerb,
+    _senObject: senObject,
+    // defines getters/setters
+    get senSubject() { return this._senSubject; },
+    set senSubject(subj) { this._senSubject = subj; },
+    get senVerb() { return this._senVerb; },
+    set senVerb(verb) { this._senVerb = verb; },
+    get senObject() { return this._senObject; },
+    set senObject(obj) { return this._senObject; }
+});
+
+// Generate sentence object with above defined arrays
+var horoscopeSentence = sentenceFactory(horoscopeSigns, msgVerb, jobTitle);
+
+// Logs to test sentence object by using the getter and printing to console the first value in each property array
+console.log(horoscopeSentence.senSubject[0]);
+console.log(horoscopeSentence.senVerb[0]);
+console.log(horoscopeSentence.senObject[0]);
